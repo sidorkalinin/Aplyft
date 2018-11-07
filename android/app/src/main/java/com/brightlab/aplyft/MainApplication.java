4e5451906bd7b183f883016b10fea93af06a1305
Ali Zain <smazaink@gmail.com>
2018-11-08 00:41:20 +0500

    Link react-native-view-shot on android

--- android/app/src/main/java/com/brightlab/aplyft/MainApplication.java
+++ android/app/src/main/java/com/brightlab/aplyft/MainApplication.java
@@ -3,6 +3,7 @@ package com.brightlab.aplyft;
 import android.app.Application;
 import com.facebook.CallbackManager;
 import com.facebook.react.ReactApplication;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import cl.json.RNSharePackage;
 import com.wog.videoplayer.VideoPlayerPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
@@ -52,6 +53,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new RNViewShotPackage(),
             new RNSharePackage(),
             new VideoPlayerPackage(),
             new ReactNativePushNotificationPackage(),
